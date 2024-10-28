bff1dd7bac51a08377a5c2afb84b5863a8d89ec9
LucasLanzanova <167720651+LucasLanzanova@users.noreply.github.com>
2024-10-28 20:47:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('revealMessage').addEventListener('click', function() {
+    const message = document.getElementById('message');
+    message.style.display = 'block';
+    message.innerHTML = 'Você é a razão do meu sorriso! 💖';
+});
